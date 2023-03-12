3314350ae2c9570c908e3158f90ec2c0cf546e50
Guilherme Tonon <gui.tonon.03@gmail.com>
2023-03-12 01:20:29 -0300

    aulas

--- Aulas/ex012/scriptAnalisador.js
+++ Aulas/ex012/scriptAnalisador.js
@@ -1,5 +1,6 @@
 var num = document.getElementsByName('num')[0]
 var sel = document.getElementById('seltab')
+var res = document.getElementById('res')
 var numeros = []
 
 
@@ -13,7 +14,13 @@ function adicionar(){
     var n = Number(num.value)
 
     if(numeros.indexOf(n) > -1){
-        //erro
+        window.alert("Valor já existente")
+        num.value = "";
+        return;
+    } else if (n > 100 || n < 1){
+        window.alert("número fora dos limites")
+        num.value = "";
+        return;
     }
 
     numeros.push(n)
@@ -25,5 +32,21 @@ function adicionar(){
 }
 
 function finalizar(){
+    var sortedNumeros = numeros.sort()
+    var soma=0
+    for(var c in numeros){
+        soma+=numeros[c]
+    }
 
+    criarDiv(`Ao todo, temos ${numeros.length} números cadastrados.`)
+    criarDiv(`O maior valor informado foi ${sortedNumeros[numeros.length-1]}.`)
+    criarDiv(`O menor valor informado foi ${sortedNumeros[0]}.`)
+    criarDiv(`Somando todos os valores, temos ${soma}.`)
+    criarDiv(`A média dos valores digitados é ${(soma/numeros.length).toFixed(2)}.`)
+}
+
+function criarDiv(text){
+    var div = document.createElement('div')
+    div.innerText = text;
+    res.appendChild(div)
 }
